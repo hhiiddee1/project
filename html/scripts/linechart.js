8a9b22cc9608f80be8b87cb665a9a84c44122a17
hhiiddee1 <hidde.van.oijen@gmail.com>
2019-01-27 16:05:20 +0100

    Update linechart.js

--- html/scripts/linechart.js
+++ html/scripts/linechart.js
@@ -5,7 +5,7 @@ var legendNames = ["Highest 20%", "4th 20%", "3rd 20%", "2nd 20%", "Lowest 20%"]
 var margin = {top: 0, right: 0, bottom: 0, left: 0},
             width = 1200 - margin.left - margin.right,
             height = 500 - margin.top - margin.bottom;
-
+var padding = 5;
 
 
 // makes Y scale
